d2614756010e5d89fb72b02767418291ab63844f
abraham ahumada <abraham.ahumada.palacin.alu@iesjulioverne.es>
2025-09-24 10:17:52 +0200

    Base de la agenda creada

---
+++ Agenda/src/main/java/com/mycompany/agenda/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenda;
+
+/**
+ *
+ * @author abraham
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
