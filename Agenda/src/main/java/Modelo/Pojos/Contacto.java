f017b463b3954fa869c9ac0ec6c9af867b6691c2
abraham ahumada <abraham.ahumada.palacin.alu@iesjulioverne.es>
2025-09-25 13:58:35 +0200

    crear atributos de contacto

--- Agenda/src/main/java/Modelo/Pojos/Contacto.java
+++ Agenda/src/main/java/Modelo/Pojos/Contacto.java
@@ -10,4 +10,28 @@ package Modelo.Pojos;
  */
 public class Contacto {
     
+    public String nombre;
+    public String apellido;
+    public String numero;
+    
+    public void Contacto(String nombre, String apellido, String numero){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numero = numero;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    
+    
 }
