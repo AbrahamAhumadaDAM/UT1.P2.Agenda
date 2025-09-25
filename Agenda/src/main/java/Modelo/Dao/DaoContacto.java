4adc4156dc21e8659803e3b0be4c53e045fe45d5
abraham ahumada <abraham.ahumada.palacin.alu@iesjulioverne.es>
2025-09-25 14:04:25 +0200

    crear arraylist en daocontacto

--- Agenda/src/main/java/Modelo/Dao/DaoContacto.java
+++ Agenda/src/main/java/Modelo/Dao/DaoContacto.java
@@ -4,10 +4,20 @@
  */
 package Modelo.Dao;
 
+import Modelo.Pojos.Contacto;
+import java.util.ArrayList;
+
 /**
  *
  * @author abraham
  */
 public class DaoContacto {
+    
+    private ArrayList<Contacto> contactos;
+    
+    public DaoContacto(){
+        contactos = new ArrayList<Contacto>();
+    }
+    
     
 }
